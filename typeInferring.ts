c3bcb1b21cf68a369013c367c583ca599f58dd1b
pain-arch <himulol123@gmail.com>
2024-05-05 18:28:32 +0600

    feat: Add type inference examples in typeInferring.ts

---
+++ typeInferring.ts
@@ -0,0 +1,12 @@
+let tech = 'typescript';
+
+tech = 123;   //not allowed and will throw an error
+tech = 'javascript'; //allowed 
+
+let age = 25;
+
+age = '25'; //not allowed and will throw an error
+age = 30; //allowed
+
+console.log(typeof tech);
+console.log(typeof age);
